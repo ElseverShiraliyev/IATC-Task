5e51245fd4ead21439794b2ae766165b8df63766
ElseverShiraliyev <125830875+ElseverShiraliyev@users.noreply.github.com>
2023-05-24 18:28:58 +0100

    Express js

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const apiKey = "2cf433b803836999fd37262569cda242";
-const city = "İsmayıllı"; // Istediyimiz sherin adini daxil edrek hava temperaturu haqqinda melumat elde ede bilerik.
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const data1 = fs.readFileSync("./index.html", "utf-8");
 
-fetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const temperature = data.main.temp;
-    console.log(`Hava temperaturu ${temperature}°C`);
-  })
-  .catch((error) => console.error(error));
+var yoda = {
+  name: "Yoda",
+  role: "Jedi Master",
+  age: 900,
+  forcePoints: 2000,
+};
+var darthmaul = {
+  name: "Darth Maul",
+  role: "Sith Lord",
+  age: 200,
+  forcePoints: 1200,
+};
+var darthFarFar = {
+  name: "Darth Far Far Wittles",
+  role: "Sith Lord",
+  age: 5,
+  forcePoints: 1000000,
+};
+
+app.get("/", async function (req, res) {
+  res.end(data1);
+});
+app.get("/yoda", async function (req, res) {
+  res.json(yoda);
+});
+
+app.get("/darthmaul", async function (req, res) {
+  res.json(darthmaul);
+});
+app.get("/farfar", async function (req, res) {
+  res.json(darthFarFar);
+});
+
+app.listen(7890, "0.0.0.0", async function () {
+  console.log("Server işləyir.");
+});
